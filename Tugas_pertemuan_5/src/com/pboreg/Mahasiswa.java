c722077eee860d4c72ef7205e4fbf638d82ae997
Irhamsyahrmdhn <71371274+Irhamsyahrmdhn@users.noreply.github.com>
2020-10-20 18:01:20 +0700

    Add files via upload

---
+++ Tugas_pertemuan_5/src/com/pboreg/Mahasiswa.java
@@ -0,0 +1,26 @@
+package com.pboreg;
+
+class Mahasiswa {
+    String nama= "Muhammad Irhamsyah Ramadhan";
+    String NIM= "201971027";
+    String jurusan= "Informatika";
+    double ipk= 3.0;
+    int umur= 19;
+
+    public String tampilkanNama(){
+        return this.nama;
+    }
+    public String tampilkanNIM(){
+        return this.NIM;
+    }
+    public String tampilkanJurusan(){
+        return this.jurusan;
+    }
+    public double tampilkanIPK(){
+        return this.ipk;
+    }
+    public int tampilkanUmur(){
+        return this.umur;
+    }
+
+}
